bd450512b57bb94a6aed94434cd6151213d72f69
diazMafer <31084398+diazMafer@users.noreply.github.com>
2018-05-11 21:33:04 -0600

    Lab terminado

---
+++ Assets/Scripts/VirtualButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButton : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    VirtualButtonBehaviour virtualBtn;
+    public Animator birdAnim;
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        birdAnim.SetTrigger("sing");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        virtualBtn = GetComponent<VirtualButtonBehaviour>();
+        virtualBtn.RegisterEventHandler(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
